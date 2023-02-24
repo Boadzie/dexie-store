a8fb42d283213b6805ef9900394918b2350f1911
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-24 15:03:28 +0000

    Move lib folder to src folder

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,18 @@
+<section class="container m-auto px-0">
+    <nav class="flex lg:flex-row flex-col gap-x-4 items-center justify-between shadow-lg p-5">
+        <ul>
+            <li class="text-4xl font-bold">Dexie-store</li>
+        </ul>
+        <ul class="uppercase flex lg:flex-row flex-col text-lg font-sans gap-x-4 items-center justify-center">
+            <li>Home</li>
+            <li>Women's</li>
+            <li>Men's</li>
+            <li>Shop</li>
+            <li>Contact</li>
+        </ul>
+        <ul class="uppercase flex text-lg font-sans gap-x-4 items-center justify-end">
+            
+            <li>Cart</li>
+        </ul>
+    </nav>
+</section>
