2704da321fcf3c3cdf667c4742063444ec92c0f0
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-24 14:35:55 +0000

    Create banner for app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="container mx-auto px-1 py-1">
+    <div class="grid grid-cols-1 lg:grid-rows-2 lg:grid-cols-4 gap-1">
+        <div class="lg:col-span-2 lg:row-span-2 relative ">
+            <img class="rounded-sm object-cover object-center h-[40.5rem] w-full"
+                src="https://preview.colorlib.com/theme/ashion/img/categories/category-1.jpg" alt="pic">
+            <div class="absolute left-10 top-[14rem]">
+                <h2 class="text-6xl text-slate-800 ">Women's Fashion</h2>
+                <p class="my-4 max-w-md text-lg text-slate-500">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque odit autem iusto velit. Cumque
+                    quisquam maiores dolor voluptatibus recusandae est id, totam nam sunt, dolorem necessitatibus omnis!
+                    Alias, facere dolore.
+                </p>
+                <button
+                    class="font-bold text-lg font-mono underline underline-offset-8 decoration-red-500 text-slate-800 uppercase">Shop
+                    now</button>
+            </div>
+        </div>
+        <div
+            class="flex gap-4 flex-col items-start justify-center col-span-1 object-cover object-right h-[20rem] w-full bg-[url('https://preview.colorlib.com/theme/ashion/img/categories/category-2.jpg')]">
+            <div class="font-sans font-bold  mx-4">
+                <h1 class="font-extrabold text-2xl text-slate-600">Men's Fashion</h1>
+                <p class="text-sm py-2 text-slate-500">435 items</p>
+                <button
+                    class="font-bold text-lg font-mono underline underline-offset-8 decoration-red-500 text-slate-800 uppercase">Shop
+                    now
+                </button>
+            </div>
+        </div>
+        <div
+            class="flex gap-4 flex-col items-start justify-center col-span-1 object-cover object-right h-[20rem] w-full bg-[url('https://preview.colorlib.com/theme/ashion/img/categories/category-3.jpg')]">
+            <div class="font-sans font-bold  mx-4">
+                <h1 class="font-extrabold text-slate-600 text-2xl ">Kid's Fashion</h1>
+                <p class="text-sm py-2 text-slate-500">43 items</p>
+                <button
+                    class="font-bold text-lg font-mono underline underline-offset-8 decoration-red-500 text-slate-800 uppercase">Shop
+                    now
+                </button>
+            </div>
+        </div>
+        <div
+            class="flex gap-4 flex-col items-start justify-center col-span-1 object-cover object-right h-[20rem] w-full bg-[url('https://preview.colorlib.com/theme/ashion/img/categories/category-4.jpg')]">
+
+            <div class="font-sans font-bold  mx-4">
+                <h1 class="font-extrabold text-slate-600 text-2xl ">Cosmetics</h1>
+                <p class="text-sm py-2 text-slate-500">200 items</p>
+                <button
+                    class="font-bold text-lg font-mono underline underline-offset-8 decoration-red-500 text-slate-800 uppercase">Shop
+                    now
+                </button>
+            </div>
+        </div>
+        <div
+            class="flex gap-4 flex-col items-start justify-center col-span-1 object-cover object-right h-[20rem] w-full bg-[url('https://preview.colorlib.com/theme/ashion/img/categories/category-5.jpg')]">
+
+            <div class="font-sans font-bold  mx-4">
+                <h1 class="font-extrabold text-slate-600 text-2xl ">Cosmetics</h1>
+                <p class="text-sm py-2 text-slate-500">200 items</p>
+                <button
+                    class="font-bold text-lg font-mono underline underline-offset-8 decoration-red-500 text-slate-800 uppercase">Shop
+                    now
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
