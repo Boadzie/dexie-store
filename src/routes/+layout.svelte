78a1fc8e2ecef99191148846e8d86002f6864aa2
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-24 15:04:13 +0000

    Add navbar component to layout page :

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script>
 	import '../app.postcss';
+	import Navbar from "$lib/Navbar.svelte";
 </script>
 
-<slot />
+<div class="flex flex-col justify-between">
+	<Navbar />
+	<main>
+		<slot />
+	</main>
+</div>
 
 
 <style>
-:global(html, body){
-	font-family: 'Cookie', cursive;
-}
+	:global(html, body) {
+		font-family: 'Cookie', cursive;
+	}
 </style>
