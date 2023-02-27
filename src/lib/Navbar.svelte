9001e5599b29fb4fb994f5488494ab534b637c17
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-27 14:46:35 +0000

    Add a cart svg to navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -14,8 +14,10 @@
             </li>
         </ul>
         <ul class="uppercase flex text-lg font-sans gap-x-4 items-center justify-end">
-            <li class="fill-red-500"><a href="/">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 icon icon-tabler icon-tabler-shopping-cart"
+            <li class="">
+                <a class="" href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        class="hover:stroke-red-500 w-6 h-6 icon icon-tabler icon-tabler-shopping-cart"
                         viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round"
                         stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -23,7 +25,8 @@
                         <circle cx="17" cy="19" r="2" />
                         <path d="M17 17h-11v-14h-2" />
                         <path d="M6 5l14 1l-1 7h-13" />
-                    </svg></a></li>
+                    </svg></a>
+            </li>
         </ul>
     </nav>
 </section>
