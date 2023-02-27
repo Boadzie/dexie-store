253083376f85eebd346098118c82e249aa1ca8ab
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-27 15:27:32 +0000

    Add cart array to NewProducts component
    - Add addToCart function that accept a product and add it to the cart

--- src/lib/NewProducts.svelte
+++ src/lib/NewProducts.svelte
@@ -8,7 +8,13 @@
             ? db.products.toArray()
             : []
     );
-    console.log(products)
+    
+
+    let cart = []
+
+    const addToCart = (product) => {
+      cart = [...cart, product]
+    }
 </script>
 
 <section class="container mx-auto px-4 py-14">
@@ -24,17 +30,22 @@
 
             <div class="relative odd:content-['sale']">
                 <img class=" p-5 rounded-sm object-cover object-conter" src={p.img_url} alt="" />
-                <h1 class=" absolute text-lg font-sans text-white top-[4rem] px-3 left-[4rem] bg-red-500 -translate-x-1/2 -translate-y-1/2">
-            SALE</h1>
+                <button on:click={addToCart(p)} class="absolute text-sm font-extrabold font-sans text-white top-[4rem] px-2 rounded-bl-md left-[4rem] bg-slate-600 hover:bg-slate-500 hover:duration-500 -translate-x-1/2 -translate-y-1/2">
+            ADD TO CART</button>
             </div>
-            <h2 class="text-2xl font-medium">{p.name}</h2>
-            <p class="text-lg text-red-600">{p.price.toLocaleString('en-US', {
+            <h2 class="text-2xl font-bold">{p.name}</h2>
+            <div class="flex gap-4 items-center justify-between">
+            <p class="text-lg font-extrabold text-slate-600">{p.price.toLocaleString('en-US', {
                 style: 'currency',
                 currency: 'USD'
-                })}</p>
+                })}
+            </p>
+            
+            </div>
         </div>
         {/each}
         {/if}
     </div>
+    
 </section>
 
