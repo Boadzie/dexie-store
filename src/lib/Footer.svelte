dbb87fb1ebccac1738c8764659484bea997f1c03
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-28 11:45:47 +0000

    Change payment images with resized ones

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,95 +1,145 @@
 <footer class="py-4 mt-5 shadow bg-white border-t-2">
-    <div class="lg:mx-12 mx-4 py-4 flex lg:flex-row flex-col gap-4 lg:justify-between items-center ">
-        <div class="flex flex-col items-start">
-            <h2 class="mb-4 text-3xl font-extrabold tracking-wider">Dexie-store</h2>
-            <p class="text-lg text-justify w-[18rem] text-slate-500">Lorem ipsum dolor sit amet, consectetur adipiscing
-                elit, sed do
-                eiusmod tempor
-                incididunt cilisis.</p>
-            <div class="my-4 flex gap-4 items-center justify-start">
-                <img class="w-18 h-18 rounded-full"
-                    src="https://preview.colorlib.com/theme/ashion/img/payment/payment-1.png" alt="" />
-                <img class="w-18 h-18 rounded-full"
-                    src="https://preview.colorlib.com/theme/ashion/img/payment/payment-2.png" alt="" />
-                <img class="w-18 h-18 rounded-full"
-                    src="https://preview.colorlib.com/theme/ashion/img/payment/payment-3.png" alt="" />
-                <img class="w-18 h-18 rounded-full"
-                    src="https://preview.colorlib.com/theme/ashion/img/payment/payment-4.png" alt="" />
-            </div>
-        </div>
-        <div>
-            <ul
-                class="flex flex-col lg:justify-start justify-center lg:items-start items-center gap-2 text-slate-500 font-sans">
-                <h2 class="my-2 text-lg text-slate-800 font-extrabold tracking-wide uppercase">Quick Links</h2>
-                <li><a href="/">Home</a> </li>
-                <li><a href="/">About</a></li>
-                <li><a href="/">Blog</a></li>
-                <li><a href="/">Contact</a></li>
-            </ul>
-        </div>
-        <div>
-            <ul class="flex justify-start lg:items-start items-center flex-col gap-2 text-slate-500 font-sans">
-                <h2 class="my-2 text-lg text-slate-800 font-extrabold tracking-wider uppercase">Shop</h2>
-                <li><a href="/">Shop</a> </li>
-                <li><a href="/">Wishlight</a></li>
-                <li><a href="/">FAQ</a></li>
-                <li><a href="/">Policy</a></li>
-            </ul>
-        </div>
-        <div class="lg:w-[20rem]">
-            <ul class="flex justify-center flex-col gap-2 text-slate-500 font-sans">
-                <h2 class="my-2 text-lg text-slate-800 font-extrabold tracking-wide uppercase">Newsletter</h2>
-                <div class="my-4">
-                    <input class="w-full relative rounded-full p-3 ring-2 ring-slate-200 focus:outline-none"
-                        type="text" />
-                    <button
-                        class="rounded-full mt-4 lg:mt-0 lg:absolute right-[3rem] bottom-[-108.045rem] px-6 bg-red-500 text-white py-[0.7rem] uppercase">Subscribe</button>
-                </div>
-                <div class="flex items-center justify-between gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        class=" w-18 h-18 rounded-full p-2 bg-slate-200 icon icon-tabler icon-tabler-brand-facebook"
-                        width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        class=" w-18 h-18 rounded-full p-2 bg-slate-200 icon icon-tabler icon-tabler-brand-twitter"
-                        width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path
-                            d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        class=" w-18 h-18 rounded-full p-2 bg-slate-200 icon icon-tabler icon-tabler-brand-youtube"
-                        width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <rect x="3" y="5" width="18" height="14" rx="4" />
-                        <path d="M10 9l5 3l-5 3z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        class="w-18 h-18 rounded-full p-2 bg-slate-200 icon icon-tabler icon-tabler-brand-instagram"
-                        width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <rect x="4" y="4" width="16" height="16" rx="4" />
-                        <circle cx="12" cy="12" r="3" />
-                        <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        class="w-18 h-18 rounded-full p-2 bg-slate-200 icon icon-tabler icon-tabler-brand-pinterest"
-                        width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <line x1="8" y1="20" x2="12" y2="11" />
-                        <path d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7" />
-                        <circle cx="12" cy="12" r="9" />
-                    </svg>
-                </div>
-            </ul>
-        </div>
-    </div>
-    <hr class="mt-4" />
+	<div class="lg:mx-12 mx-4 py-4 flex lg:flex-row flex-col gap-4 lg:justify-between items-center ">
+		<div class="flex flex-col items-start">
+			<h2 class="mb-4 text-3xl font-extrabold tracking-wider">Dexie-store</h2>
+			<p class="text-lg text-justify w-[18rem] text-slate-500">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+				cilisis.
+			</p>
+			<div class="my-4 flex gap-4 items-center justify-start">
+				<img class="w-18 h-18 rounded-full" src="img/payment-1.png" alt="" />
+				<img class="w-18 h-18 rounded-full" src="img/payment-2.png" alt="" />
+				<img class="w-18 h-18 rounded-full" src="img/payment-3.png" alt="" />
+				<img class="w-18 h-18 rounded-full" src="img/payment-4.png" alt="" />
+			</div>
+		</div>
+		<div>
+			<ul
+				class="flex flex-col lg:justify-start justify-center lg:items-start items-center gap-2 text-slate-500 font-sans"
+			>
+				<h2 class="my-2 text-lg text-slate-800 font-extrabold tracking-wide uppercase">
+					Quick Links
+				</h2>
+				<li><a href="/">Home</a></li>
+				<li><a href="/">About</a></li>
+				<li><a href="/">Blog</a></li>
+				<li><a href="/">Contact</a></li>
+			</ul>
+		</div>
+		<div>
+			<ul
+				class="flex justify-start lg:items-start items-center flex-col gap-2 text-slate-500 font-sans"
+			>
+				<h2 class="my-2 text-lg text-slate-800 font-extrabold tracking-wider uppercase">Shop</h2>
+				<li><a href="/">Shop</a></li>
+				<li><a href="/">Wishlight</a></li>
+				<li><a href="/">FAQ</a></li>
+				<li><a href="/">Policy</a></li>
+			</ul>
+		</div>
+		<div class="lg:w-[20rem]">
+			<ul class="flex justify-center flex-col gap-2 text-slate-500 font-sans">
+				<h2 class="my-2 text-lg text-slate-800 font-extrabold tracking-wide uppercase">
+					Newsletter
+				</h2>
+				<div class="my-4">
+					<input
+						class="w-full relative rounded-full p-3 ring-2 ring-slate-200 focus:outline-none"
+						type="text"
+					/>
+					<button
+						class="rounded-full mt-4 lg:mt-0 lg:absolute right-[3rem] bottom-[-108.045rem] px-6 bg-red-500 text-white py-[0.7rem] uppercase"
+						>Subscribe</button
+					>
+				</div>
+				<div class="flex items-center justify-between gap-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class=" w-18 h-18 rounded-full p-2 bg-slate-200 icon icon-tabler icon-tabler-brand-facebook"
+						width="44"
+						height="44"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="#2c3e50"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class=" w-18 h-18 rounded-full p-2 bg-slate-200 icon icon-tabler icon-tabler-brand-twitter"
+						width="44"
+						height="44"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="#2c3e50"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path
+							d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z"
+						/>
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class=" w-18 h-18 rounded-full p-2 bg-slate-200 icon icon-tabler icon-tabler-brand-youtube"
+						width="44"
+						height="44"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="#2c3e50"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<rect x="3" y="5" width="18" height="14" rx="4" />
+						<path d="M10 9l5 3l-5 3z" />
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-18 h-18 rounded-full p-2 bg-slate-200 icon icon-tabler icon-tabler-brand-instagram"
+						width="44"
+						height="44"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="#2c3e50"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<rect x="4" y="4" width="16" height="16" rx="4" />
+						<circle cx="12" cy="12" r="3" />
+						<line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-18 h-18 rounded-full p-2 bg-slate-200 icon icon-tabler icon-tabler-brand-pinterest"
+						width="44"
+						height="44"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="#2c3e50"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<line x1="8" y1="20" x2="12" y2="11" />
+						<path
+							d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7"
+						/>
+						<circle cx="12" cy="12" r="9" />
+					</svg>
+				</div>
+			</ul>
+		</div>
+	</div>
+	<hr class="mt-4" />
 </footer>
