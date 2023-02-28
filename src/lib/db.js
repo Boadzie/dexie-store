dd0f2a634253b59504b0527e06b13f6d46571d74
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-28 11:46:43 +0000

    Add Add new version of db and add new products

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,27 +1,28 @@
 import Dexie from 'dexie';
 
 export const db = new Dexie('dexieStoreDB');
-db.version(1).stores({
-	products: '++id, name, price, img_url, rating' // Primary key and indexed props
+db.version(2).stores({
+	products: '++id, name, price, img_url, rating',
+	cart: '++id, name, price, img_url, rating, quantity' // Primary key and indexed props
 });
 
 // db.products.bulkAdd([
 // 	{
-// 		name: 'T-skirt',
-// 		price: 54.9,
-// 		img_url: 'https://preview.colorlib.com/theme/ashion/img/product/product-5.jpg',
+// 		name: 'Buttons blazer',
+// 		price: 74.9,
+// 		img_url: 'img/product-1.jpg',
+// 		rating: 2.5
+// 	},
+// 	{
+// 		name: 'Flowy  skirt',
+// 		price: 38.3,
+// 		img_url: 'img/product-6.jpg',
+// 		rating: 3.5
+// 	},
+// 	{
+// 		name: 'Cotton Bag',
+// 		price: 29.4,
+// 		img_url: 'img/product-2.jpg',
 // 		rating: 4.5
-// 	},
-// 	{
-// 		name: 'T-shirt',
-// 		price: 64.9,
-// 		img_url: 'https://preview.colorlib.com/theme/ashion/img/product/product-6.jpg',
-// 		rating: 3.5
-// 	},
-// 	{
-// 		name: 'Polo Top',
-// 		price: 64.9,
-// 		img_url: 'https://preview.colorlib.com/theme/ashion/img/product/product-7.jpg',
-// 		rating: 3.5
 // 	}
 // ]);
